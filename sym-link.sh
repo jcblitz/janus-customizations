7de7d038695ac47cd89b31eb499b52833ed40a21
Jared Blitzstein <jblitzstein@ebay.com>
2015-04-01 21:03:10 -0400

    Added basic stuff back in

---
+++ sym-link.sh
@@ -0,0 +1,5 @@
+cd ~
+ln -s ~/.janus-customizations/.gvimrc.after
+ln -s ~/.janus-customizaitons/.gvimrc.before
+ln -s ~/.janus-customizaitons/.vimrc.after
+ln -s ~/.janus-customizaitons/.vimrc.before
